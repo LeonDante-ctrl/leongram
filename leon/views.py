bae0cc418e3c062e38b4bc83b86099c06497e5b1
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-07 14:38:27 +0300

    made sure my models were properly incorperated to show additions to the page through the admin page , now to making the page more interactive to the user

--- leon/views.py
+++ leon/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post 
+from django.http import HttpResponse
+from django.views.generic import (
+    ListView
+)
 
-# Create your views here.
+class PostListView(ListView):
+    template_name = 'pages/postlist.html'
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
