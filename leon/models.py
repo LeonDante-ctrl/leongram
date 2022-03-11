803ac4bc4bf9e560e50cc1e020bce28003f8ab08
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-11 14:01:31 +0300

    added some new models to my models.py that would facilitate the functioning of my saving option as well as my caption option and my likes section

--- leon/models.py
+++ leon/models.py
@@ -1,4 +1,5 @@
 from time import timezone
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
@@ -10,6 +11,8 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     image = models.ImageField(blank=True, null =True)
     caption = models.TextField()
+    saved = models.BooleanField(default=False)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='post_likes')
     created_period = models.DateTimeField(default=timezone.now)
     
     def get_absolute_url(self):
