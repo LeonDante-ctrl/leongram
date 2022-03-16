fcd4e404dfb70fa0b9e9a1db2ec0ffce41b08e5d
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-16 20:00:42 +0300

    pushing app

--- leongram/settings.py
+++ leongram/settings.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
