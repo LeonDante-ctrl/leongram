967b16137eca1f51a80bdfc5c1138c679399c63d
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-16 10:50:15 +0300

    updated code

--- leon/urls.py
+++ leon/urls.py
@@ -13,7 +13,7 @@ app_name = 'leon'
 urlpatterns = [
     #local : http://127.0.0.1:8000/
     path('',PostListView.as_view(), name='post_list'),
-    path('new/', PostCreateView.as_view(), name = 'post_create'),
+    path('new/', PostCreateView.as_view(), name = 'create'),
     path('<int:id>' , PostDetailView.as_view(), name='post_detail'),
     path('<int:id>/update/', PostUpdateView.as_view(), name='post_update'),
     path('<int:id>/delete/', PostDeleteView.as_view(), name='post_delete'),
